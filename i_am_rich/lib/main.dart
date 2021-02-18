d390fa4eb31a3d47aec4e5979096ed279d36bd4f
Carlo Munguia <carlo.munguia.82@gmail.com>
2021-02-17 23:33:09 -0600

    I Am Zen: Centering
    centered image, using handy centering scaffold trick

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -4,12 +4,18 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-          backgroundColor: Colors.amber[500],
-          appBar: AppBar(
-              title: Text('I am Zen'),
-              centerTitle: true,
-              backgroundColor: Colors.blueGrey[900]),
-          body: Image()),
+        backgroundColor: Colors.blueGrey[900],
+        appBar: AppBar(
+            title: Text('I am Zen'),
+            centerTitle: true,
+            backgroundColor: Colors.blueGrey[900]),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjsGiaT84pAfF0f6SjWCsOU6zR39rzMpWG7SSfLHL90TBpPwRvUQii37yO7IhIyXrzYo7Wa1ED&usqp=CAc'),
+          ),
+        ),
+      ),
     ),
   );
 }
